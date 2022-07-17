0a1929b34e91f4e231de6d34e05d4db53bd6a80a
Draacckeer <u201922975@upc.edu.pe>
2022-07-16 23:57:53 -0500

    Update UserServiceImpl.java

--- src/main/java/com/example/fullstacksocialapp_api/security/services/UserServiceImpl.java
+++ src/main/java/com/example/fullstacksocialapp_api/security/services/UserServiceImpl.java
@@ -196,7 +196,6 @@ public class UserServiceImpl implements UserService {
                     likesList.add(userToLike);
                     user.setLikesList(likesList);
                     userToLike.setLikes(userToLike.getLikes() + 1);
-                    userRepository.saveAndFlush(user);
                     UserResource resource = mapper.map(userToLike, UserResource.class);
                     return ResponseEntity.ok(resource);
                 }
