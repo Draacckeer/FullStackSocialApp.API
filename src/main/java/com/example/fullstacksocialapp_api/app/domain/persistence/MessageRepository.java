27655d958d4ebdc440f1ba74d124593f4f4003d0
Draacckeer <u201922975@upc.edu.pe>
2022-07-18 23:49:56 -0500

    Update MessageRepository.java

--- src/main/java/com/example/fullstacksocialapp_api/app/domain/persistence/MessageRepository.java
+++ src/main/java/com/example/fullstacksocialapp_api/app/domain/persistence/MessageRepository.java
@@ -2,6 +2,7 @@ package com.example.fullstacksocialapp_api.app.domain.persistence;
 
 import com.example.fullstacksocialapp_api.app.domain.model.entity.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +11,8 @@ import java.util.List;
 public interface MessageRepository extends JpaRepository<Message, Long> {
     List<Message> findByUserSenderid(Long userSenderid);
     List<Message> findByUserReceiverid(Long userReceiverid);
+
+    @Query(value = "SELECT * FROM Message m WHERE m.user_receiverid = 2 AND m.user_senderid=1 OR m.user_senderid=2 AND m.user_receiverid=1",
+            nativeQuery = true)
     List<Message> findByUserSenderidAndUserReceiverid(Long userSenderid, Long userReceiverid);
 }
