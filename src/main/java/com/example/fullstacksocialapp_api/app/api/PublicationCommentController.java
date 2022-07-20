be1b7455a5f40863f040c8028a62657da30097e9
Draacckeer <u201922975@upc.edu.pe>
2022-07-20 02:23:43 -0500

    Deploy

--- src/main/java/com/example/fullstacksocialapp_api/app/api/PublicationCommentController.java
+++ src/main/java/com/example/fullstacksocialapp_api/app/api/PublicationCommentController.java
@@ -17,6 +17,7 @@ import java.util.List;
 @RestController
 @RequestMapping("api/v1/publication_comments")
 public class PublicationCommentController {
+    
     private final PublicationCommentService publicationCommentService;
     private final PublicationCommentMapper mapper;
     public PublicationCommentController(PublicationCommentService publicationCommentService, PublicationCommentMapper mapper){
